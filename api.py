42582cf62901cf6ec609c135dda70344209dd104
sci17 <recarzekaycee@gmail.com>
2024-12-13 17:10:57 +0700

    created an update feature

--- api.py
+++ api.py
@@ -16,7 +16,7 @@ mysql = MySQL(app)
 def hello_world():
     return "<p>Hello, World</p>"
 
-# customers 
+# CUSTOMERS CRUD
 @app.route("/customers", methods=["GET"])
 def get_customers():
     cur = mysql.connection.cursor()
@@ -58,6 +58,27 @@ def add_customer():
     rows_affected = cur.rowcount
     return make_response(jsonify({"message": "customer added successfully", "rows_affected":rows_affected}), 201)
 
+@app.route("/customers/<int:id>", methods=["PUT"])
+def update_customer(id):
+    cur = mysql.connection.cursor()
+    info = request.get_json()
+    first_name = info["first_name"]
+    last_name = info["last_name"]
+    address = info["address"]
+    phone_number = info["phone_number"]
+    cell_mobile = info["cell_mobile"]
+    email_address = info["email_address"]
+    other_details = info["other_details"]
+
+    cur.execute("""
+        UPDATE customers SET first_name = %s, last_name = %s, address = %s, phone_number = %s, cell_mobile = %s, email_address = %s, other_details = %s
+                WHERE customer_id = %s""", (first_name, last_name,address, phone_number, cell_mobile, email_address, other_details,id)
+    )
+    print("row(s) affected: {}".format(cur.rowcount))
+    rows_affected = cur.rowcount
+    mysql.connection.commit()
+    return make_response(jsonify({"message": "customer updated successfuly", "rows_affected": rows_affected}), 201)
+
   
 # accounts
 @app.route("/accounts", methods=["GET"])
@@ -87,21 +108,8 @@ def get_accounts():
 def get_account_id(id):
     cur = mysql.connection.cursor()
     query = """
-    SELECT 
-    a.account_id, 
-    a.account_name, 
-    pm.payment_method_code, 
-    c.customer_id, 
-    a.account_details
-    FROM 
-        accounts AS a
-    INNER JOIN 
-        payment_methods AS pm
-        ON a.payment_method_code = pm.payment_method_code  
-    INNER JOIN 
-        customers AS c
-        ON a.customer_id = c.customer_id  
-    WHERE a.account_id = {}
+    SELECT * from accounts
+    WHERE account_id = {}
     """.format(id)
     cur.execute(query)
     data = cur.fetchall()
@@ -125,6 +133,23 @@ def add_accounts():
     rows_affected = cur.rowcount
     return make_response(jsonify({"message": "accounts added successfully", "rows_affected":rows_affected}), 201)
 
+@app.route("/accounts/<int:id>", methods=["PUT"])
+def update_accounts(id):
+    cur = mysql.connection.cursor()
+    info = request.get_json()
+    account_name = info["account_name"]
+    payment_method_code = info["payment_method_code"]
+    customer_id = info["customer_id"]
+    account_details = info["account_details"]
+
+    cur.execute("""
+        UPDATE accounts SET account_name = %s, payment_method_code = %s, customer_id = %s, account_details = %s
+                WHERE account_id = %s""", (account_name, payment_method_code,customer_id, account_details,id)
+    )
+    print("row(s) affected: {}".format(cur.rowcount))
+    rows_affected = cur.rowcount
+    mysql.connection.commit()
+    return make_response(jsonify({"message": "accounts updated successfuly", "rows_affected": rows_affected}), 201)
 
 
 # financial
@@ -169,6 +194,28 @@ def add_financial():
     print("row(s) affected: {}".format(cur.rowcount))
     rows_affected = cur.rowcount
     return make_response(jsonify({"message": "financial transaction added successfully", "rows_affected":rows_affected}), 201)
+
+@app.route("/financial/<int:id>", methods=["PUT"])
+def update_financial(id):
+    cur = mysql.connection.cursor()
+    info = request.get_json()
+    account_id = info["account_id"]
+    item_rental_id = info["item_rental_id"]
+    purchase_id = info["purchase_id"]
+    previous_transaction_id = info["previous_transaction_id"]
+    transaction_date = info["transaction_date"]
+    transaction_type_code = info["transaction_type_code"]
+    transaction_amount = info["transaction_amount"]
+    comment = info["comment"]
+
+    cur.execute("""
+        UPDATE financial_transactions SET account_id = %s, item_rental_id = %s, purchase_id = %s, previous_transaction_id = %s, transaction_date = %s, transaction_type_code = %s, transaction_amount = %s, comment = %s
+                WHERE transaction_id = %s""", (account_id, item_rental_id,purchase_id, previous_transaction_id, transaction_date, transaction_type_code, transaction_amount,comment,id)
+    )
+    print("row(s) affected: {}".format(cur.rowcount))
+    rows_affected = cur.rowcount
+    mysql.connection.commit()
+    return make_response(jsonify({"message": "financial transaction updated successfuly", "rows_affected": rows_affected}), 201)
 
 
 # purchases
@@ -241,6 +288,27 @@ def add_purchases():
     rows_affected = cur.rowcount
     return make_response(jsonify({"message": "item purchases transaction added successfully", "rows_affected":rows_affected}), 201)
 
+@app.route("/purchases/<int:id>", methods=["PUT"])
+def update_purchase(id):
+    cur = mysql.connection.cursor()
+    info = request.get_json()
+    purchase_status_code = info["purchase_status_code"]
+    customer_id = info["customer_id"]
+    item_id = info["item_id"]
+    purchase_date = info["purchase_date"]
+    purchase_quantity = info["purchase_quantity"]
+    amount_due = info["amount_due"]
+
+    cur.execute("""
+        UPDATE customer_item_purchases SET purchase_status_code = %s, customer_id = %s, item_id = %s, purchase_date = %s, purchase_quantity = %s, amount_due = %s
+                WHERE purchase_id = %s""", (purchase_status_code, customer_id,item_id, purchase_date, purchase_quantity, amount_due,id)
+    )
+    print("row(s) affected: {}".format(cur.rowcount))
+    rows_affected = cur.rowcount
+    mysql.connection.commit()
+    return make_response(jsonify({"message": "customer purchases updated successfuly", "rows_affected": rows_affected}), 201)
+
+
 # rentals
 @app.route("/rentals", methods=["GET"])
 def get_rentals():
@@ -282,6 +350,27 @@ def add_rentals():
     rows_affected = cur.rowcount
     return make_response(jsonify({"message": "rental transaction added successfully", "rows_affected":rows_affected}), 201)
 
+@app.route("/rentals/<int:id>", methods=["PUT"])
+def update_rentals(id):
+    cur = mysql.connection.cursor()
+    info = request.get_json()
+    customer_id = info["customer_id"]
+    item_id = info["item_id"]
+    rental_date_out = info["rental_date_out"]
+    rental_date_returned = info["rental_date_returned"]
+    amount_due = info["amount_due"]
+    other_details = info["other_details"]
+
+    cur.execute("""
+        UPDATE customer_item_rentals SET customer_id = %s, item_id = %s, rental_date_out = %s, rental_date_returned = %s, amount_due = %s, other_details = %s
+                WHERE item_rental_id = %s""", (customer_id, item_id,rental_date_out, rental_date_returned, amount_due, other_details,id)
+    )
+    print("row(s) affected: {}".format(cur.rowcount))
+    rows_affected = cur.rowcount
+    mysql.connection.commit()
+    return make_response(jsonify({"message": "customer rental updated successfuly", "rows_affected": rows_affected}), 201)
+
+
 # item type
 @app.route("/item_type", methods=["GET"])
 def get_item_type():
@@ -297,7 +386,7 @@ def get_item_type():
 def get_item_type_id(id):
     cur = mysql.connection.cursor()
     query = """
-    select * from inventory_item_typrs where item_type_code = {}
+    select * from inventory_item_types where item_type_code = {}
     """.format(id)
     cur.execute(query)
     data = cur.fetchall()
@@ -318,6 +407,22 @@ def add_item_type():
     print("row(s) affected: {}".format(cur.rowcount))
     rows_affected = cur.rowcount
     return make_response(jsonify({"message": "item type transaction added successfully", "rows_affected":rows_affected}), 201)
+
+@app.route("/item_type/<int:id>", methods=["PUT"])
+def update_item_type(id):
+    cur = mysql.connection.cursor()
+    info = request.get_json()
+    item_type_code = info["item_type_code"]
+    description = info["description"]
+
+    cur.execute("""
+        UPDATE inventory_item_types SET item_type_code = %s, description = %s
+                WHERE item_type_code = %s""", (item_type_code, description,id)
+    )
+    print("row(s) affected: {}".format(cur.rowcount))
+    rows_affected = cur.rowcount
+    mysql.connection.commit()
+    return make_response(jsonify({"message": "item type updated successfuly", "rows_affected": rows_affected}), 201)
 
 # items
 @app.route("/items", methods=["GET"])
@@ -361,6 +466,27 @@ def add_items():
     rows_affected = cur.rowcount
     return make_response(jsonify({"message": "items added successfully", "rows_affected":rows_affected}), 201)
 
+@app.route("/items/<int:id>", methods=["PUT"])
+def update_items(id):
+    cur = mysql.connection.cursor()
+    info = request.get_json()
+    item_id = info["item_id"]
+    item_type_code = info["item_type_code"]
+    description = info["description"]
+    number_in_stocks = info["number_in_stocks"]
+    rental_sale_both = info["rental_sale_both"]
+    rental_daily_rate = info["rental_daily_rate"]
+    sale_price = info["sale_price"]
+
+    cur.execute("""
+        UPDATE inventory_items SET item_id = %s, item_type_code = %s, description = %s, number_in_stocks = %s, rental_sale_both = %s, rental_daily_rate = %s, sale_price = %s
+                WHERE item_id = %s""", (item_id,item_type_code, description,number_in_stocks, rental_sale_both,rental_daily_rate, sale_price,id)
+    )
+    print("row(s) affected: {}".format(cur.rowcount))
+    rows_affected = cur.rowcount
+    mysql.connection.commit()
+    return make_response(jsonify({"message": "items updated successfuly", "rows_affected": rows_affected}), 201)
+
 # purchase_status_code
 @app.route("/status_code", methods=["GET"])
 def get_status_code():
@@ -376,7 +502,7 @@ def get_status_code():
 def get_status_code_id(id):
     cur = mysql.connection.cursor()
     query = """
-    select * from purchase_status_code where purchase_status_code = {}
+    select * from purchase_status_codes where purchase_status_code = {}
     """.format(id)
     cur.execute(query)
     data = cur.fetchall()
@@ -399,6 +525,23 @@ def add_status_code():
     rows_affected = cur.rowcount
     return make_response(jsonify({"message": "status code added successfully", "rows_affected":rows_affected}), 201)
 
+@app.route("/status_code/<int:id>", methods=["PUT"])
+def update_status_code(id):
+    cur = mysql.connection.cursor()
+    info = request.get_json()
+    purchase_status_code = info["purchase_status_code"]
+    description = info["description"]
+
+    cur.execute("""
+        UPDATE purchase_status_codes SET purchase_status_code = %s, description = %s
+                WHERE purchase_status_code = %s""", (purchase_status_code, description,id)
+    )
+    print("row(s) affected: {}".format(cur.rowcount))
+    rows_affected = cur.rowcount
+    mysql.connection.commit()
+    return make_response(jsonify({"message": "status code updated successfuly", "rows_affected": rows_affected}), 201)
+
+
 #  transaction types
 @app.route("/transaction", methods=["GET"])
 def get_transaction():
@@ -414,7 +557,7 @@ def get_transaction():
 def get_transaction_id(id):
     cur = mysql.connection.cursor()
     query = """
-    select * from financial_transactions where transaction_id = {}
+    select * from transaction_types where transaction_type_code = {}
     """.format(id)
     cur.execute(query)
     data = cur.fetchall()
@@ -436,6 +579,22 @@ def add_transaction():
     rows_affected = cur.rowcount
     return make_response(jsonify({"message": "transaction type added successfully", "rows_affected":rows_affected}), 201)
 
+@app.route("/transaction/<int:id>", methods=["PUT"])
+def update_transaction(id):
+    cur = mysql.connection.cursor()
+    info = request.get_json()
+    transaction_type_code = info["transaction_type_code"]
+    description = info["description"]
+
+    cur.execute("""
+        UPDATE transaction_types SET transaction_type_code = %s, description = %s
+                WHERE transaction_type_code = %s""", (transaction_type_code, description,id)
+    )
+    print("row(s) affected: {}".format(cur.rowcount))
+    rows_affected = cur.rowcount
+    mysql.connection.commit()
+    return make_response(jsonify({"message": "transaction type updated successfuly", "rows_affected": rows_affected}), 201)
+
 
 # payment methods
 @app.route("/payment", methods=["GET"])
@@ -448,7 +607,7 @@ def get_payment():
     data = cur.fetchall()
     return make_response(jsonify(data), 200)
 
-@app.route("/payments/<int:id>", methods=["GET"])
+@app.route("/payment/<int:id>", methods=["GET"])
 def get_payment_id(id):
     cur = mysql.connection.cursor()
     query = """
@@ -458,7 +617,7 @@ def get_payment_id(id):
     data = cur.fetchall()
     return make_response(jsonify(data), 200)
 
-@app.route("/payments", methods=["POST"])
+@app.route("/payment", methods=["POST"])
 def add_payments():
     cur = mysql.connection.cursor()
     info = request.get_json()
@@ -473,6 +632,22 @@ def add_payments():
     print("row(s) affected: {}".format(cur.rowcount))
     rows_affected = cur.rowcount
     return make_response(jsonify({"message": "transaction type added successfully", "rows_affected":rows_affected}), 201)
+
+@app.route("/payment/<int:id>", methods=["PUT"])
+def update_payments(id):
+    cur = mysql.connection.cursor()
+    info = request.get_json()
+    payment_method_code = info["payment_method_code"]
+    description = info["description"]
+
+    cur.execute("""
+        UPDATE payment_methods SET payment_method_code = %s, description = %s
+                WHERE payment_method_code = %s""", (payment_method_code, description,id)
+    )
+    print("row(s) affected: {}".format(cur.rowcount))
+    rows_affected = cur.rowcount
+    mysql.connection.commit()
+    return make_response(jsonify({"message": "payment method updated successfuly", "rows_affected": rows_affected}), 201)
 
 
 
